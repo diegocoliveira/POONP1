a316adf0b3149c4ee1534c19246c48634b38d627
Daniel do Carmo <danielt.docarmo@gmail.com>
2017-04-01 14:08:06 -0300

    Coloca main na classe AgendamentoVoo

--- src/com/company/AgendamentoVoo.java
+++ src/com/company/AgendamentoVoo.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class AgendamentoVoo {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
